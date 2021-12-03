d78945583ee7b030a1bc3ae8ab7076b40a17ab78
evo <84800113+evo0616lution@users.noreply.github.com>
2021-12-03 10:22:57 +0100

    Create server-count-in-status.py

---
+++ server-count-in-status.py
@@ -0,0 +1,11 @@
+#Import discord, discord.ext commands and tasks before running
+@tasks.loop(seconds=300.0)
+async def refresh():
+  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{len(bot.guilds)} servers | +help"))
+
+@bot.event
+async def on_ready():
+  time.sleep(1)
+  print(f"Ready on {len(bot.guilds)} servers!")
+  await bot.wait_until_ready()
+  refresh.start()
